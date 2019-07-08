8a949293eb1e4d63aaaaaf3a31f7a0a9c095d31a
Bello Osagie <osagienoah04@gmail.com>
2019-07-08 18:22:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,15 @@ return results;
 }
 
 theBeatlesPlay(["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"],["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]);
+
+function johnLennonFacts(array){
+  var i = 0;
+  var results =[];
+  while(array[i]){
+    results.push(array[i]+"!!!");
+    i++;
+  }
+  return results;
+}
+
+johnLennonFacts([ 'John Lennon plays Guitar ', 'PaulMcCartney plays Bass Guitar ', 'George Harrison plays LeadGuitar ', 'Ringo Starr plays Drums ' ] )
