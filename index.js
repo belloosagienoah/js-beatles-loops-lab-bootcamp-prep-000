4eefda99c18a0d371ea333a0367ebd20213522f5
Bello Osagie <osagienoah04@gmail.com>
2019-07-08 18:19:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // add solution here
+function theBeatlesPlay(arrayMusicians, arrayInstruments){
+var results =[];
+for(var i=0; i<arrayMusicians.length; i++){
+
+    results.push(`${arrayMusicians[i]} plays ${arrayInstruments[i]}`);
+}
+
+return results;
+}
+
+theBeatlesPlay(["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"],["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]);
